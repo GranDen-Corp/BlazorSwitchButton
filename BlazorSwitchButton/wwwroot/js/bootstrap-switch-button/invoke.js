cb2bbd5549e6f231e008b6fd45f904760e1a1449
Chen-Yu Pao <windperson@gmail.com>
2020-12-27 22:07:43 +0800

    Add source files

---
+++ BlazorSwitchButton/wwwroot/js/bootstrap-switch-button/invoke.js
@@ -0,0 +1,7 @@
+﻿export function createSwitchButton (switchBtnContainer) {
+   let input = document.createElement('input');
+   input.setAttribute('type', 'checkbox');
+   input.checked = true;
+   switchBtnContainer.appendChild(input);
+   input.switchButton();
+}
