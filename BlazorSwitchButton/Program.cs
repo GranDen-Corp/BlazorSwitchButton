bfb6501fdd21d8eef4ab3805aaaa21d95cfe5c23
Chen-Yu Pao <windperson@gmail.com>
2020-12-28 13:25:26 +0800

    Workable Hook event code

--- BlazorSwitchButton/Program.cs
+++ BlazorSwitchButton/Program.cs
@@ -4,8 +4,10 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Serilog;
 
 namespace BlazorSwitchButton
 {
@@ -13,11 +15,34 @@ namespace BlazorSwitchButton
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .AddJsonFile("appsettings.Development.json", true)
+                .Build();
+
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(configuration)
+                .CreateLogger();
+            try
+            {
+
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex, "Host terminated unexpectedly");
+                throw;
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseSerilog()
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
---
+++ BlazorSwitchButton/wwwroot/js/bootstrap-switch-button/hookEvent.js
@@ -0,0 +1,14 @@
+﻿let dotNetInvokeReference = {
+    dotNetReference: null,
+    init: function (dotNetReference, checkBoxReference) {
+        this.dotNetReference = dotNetReference;
+        checkBoxReference.addEventListener('change', function (event) {
+            event.value = checkBoxReference.checked;
+            dotNetReference.invokeMethodAsync('SwitchBtnEventHandler', event)
+        });
+    }
+};
+
+export let createDotNetInvokeRef = function () {
+    return dotNetInvokeReference
+};
