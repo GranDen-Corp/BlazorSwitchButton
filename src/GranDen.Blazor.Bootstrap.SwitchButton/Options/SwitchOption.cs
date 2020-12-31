f8e3e6b9f5296bb7e2255c5e2181de2643234e4e
Chen-Yu Pao <windperson@gmail.com>
2021-01-01 04:08:54 +0800

    Fix wrong css loading statement

---
+++ src/GranDen.Blazor.Bootstrap.SwitchButton/Options/SwitchOption.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GranDen.Blazor.Bootstrap.SwitchButton.Options
+{
+    public class SwitchOption
+    {
+        public string Onlabel { get; set; } = "On";
+
+        public string Offlabel { get; set; } = "Off";
+
+        public string Onstyle { get; set; } = "primary";
+
+        public string Offstyle { get; set; } = "light";
+
+        public string Size { get; set; } = null;
+
+        public string Style { get; set; } = null;
+
+        public int? Width { get; set; }
+
+        public int? Height { get; set; }
+    }
+}
